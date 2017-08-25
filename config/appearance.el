d5abf539bb9dfdd4f812c3d2e1825a547b1c31ef
Pavan Kumar L <pavankumarlpersonal@gmail.com>
2017-08-25 21:05:44 +0530

    Updates
    - bug fixes
    - org mode cleanup

--- config/appearance.el
+++ config/appearance.el
@@ -23,15 +23,15 @@
 (use-package all-the-icons)
 
 ;; install monokai theme
-(use-package doom-themes
+(use-package monokai-theme
   :ensure t
   :config
-  (load-theme 'monokai t)
-   (setq monokai-height-minus-1 0.8
-        monokai-height-plus-1 1.1
-        monokai-height-plus-2 1.15
-        monokai-height-plus-3 1.2
-        monokai-height-plus-4 1.3))
+  (setq monokai-height-minus-1 1.0
+        monokai-height-plus-1 1.0
+        monokai-height-plus-2 1.0
+        monokai-height-plus-3 1.0
+        monokai-height-plus-4 1.0)
+  (load-theme 'monokai t))
 
 (use-package all-the-icons)
 
--- config/org-settings.el
+++ config/org-settings.el
@@ -1,74 +1,33 @@
+;;; package --- summary
+;;; commentary:
+;;; contains org mode settings and customizations
+
+;;; code:
 (use-package org-bullets
   :ensure t
   :config
-  (org-bullets-mode 1))
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
 (custom-set-variables
+ '(org-directory "~/Dropbox/personal")
  '(org-default-notes-file (concat org-directory "/notes.org"))
- '(org-directory "~/Dropbox/everythingme")
- '(org-export-html-postamble nil)
- '(org-hide-leading-stars t)
- '(org-startup-folded (quote overview))
- '(org-startup-indented t)
- )
+ '(org-todo-keyword-faces
+       '(("TODO" . "#A64E4E")
+	 ("SOMEDAY" . "orange")
+	 ("IN_PROGRESS" . "#2C95FF")
+	 ("DONE" . "#11AE13")
+	 ("CANCELLED" . "#BFCFD9")))
+ '(org-todo-keywords
+   '((sequence "TODO(t)" "SOMEDAY(s)" "IN_PROGRESS(p)"  "|" "DONE(d)" "CANCELLED(c)")))
+ '(org-agenda-files (list "~/Dropbox/personal/my-life.org"))
+ '(org-capture-templates
+      '(("j" "Journal" entry (file+datetree "~/Dropbox/everythingme/journal.org")
+	 "* %?\nEntered on %U\n  %i\n  %a"))))
 
+;;; org-agenda shortcut
 (global-set-key (kbd "<f6>") 'org-agenda)
 
-(require 'ox-odt)
+;;; org-capture shortcut
+(global-set-key (kbd "<f7>") 'org-capture)
 
-;;;;;;;; org-capture settings
-  (global-set-key (kbd "<f7>") 'org-capture)
-
-
-  (setq org-agenda-files (list "~/Dropbox/everythingme/gcal.org"
-                               "~/Dropbox/everythingme/i.org"))
-
-
-  (setq org-capture-templates
-        '(("a" "Appointment" entry (file+headline  "~/Dropbox/everythingme/gcal.org" "Appointments")
-           "* TODO %?\n:PROPERTIES:\n\n:END:\nDEADLINE: %^T \n %i\n")
-          ("n" "Note" entry (file+headline "~/Dropbox/everythingme/notes.org" "Notes")
-           "* Note %?\n%T")
-           ("l" "Link" entry (file+headline "~/Dropbox/everythingme/links.org" "Links")
-           "* %? %^L %^g \n%T" :prepend t)
-          ("t" "To Do Item" entry (file+headline "~/Dropbox/everythingme/i.org" "To Do Items")
-           "* %?\n%T" :prepend t)
-	  ("d" "Day to day tasks" entry (file+datetree "~/Dropbox/everythingme/day-to-day.org")
-	   "**** TODO %i%? \n%U")
-          ("j" "Journal" entry (file+datetree "~/Dropbox/everythingme/journal.org")
-           "* %?\nEntered on %U\n  %i\n  %a")))
-
-
-  (defadvice org-capture-finalize 
-      (after delete-capture-frame activate)  
-    "Advise capture-finalize to close the frame"  
-    (if (equal "capture" (frame-parameter nil 'name))  
-        (delete-frame)))
-
-  (defadvice org-capture-destroy 
-    (after delete-capture-frame activate)  
-     "Advise capture-destroy to close the frame"  
-     (if (equal "capture" (frame-parameter nil 'name))  
-         (delete-frame)))  
-
-  (use-package noflet
-  :ensure t )
-  (defun make-capture-frame ()
-           "Create a new frame and run org-capture."
-           (interactive)
-           (make-frame '((name . "capture")))
-           (select-frame-by-name "capture")
-           (delete-other-windows)
-           (noflet ((switch-to-buffer-other-window (buf) (switch-to-buffer buf)))
-             (org-capture)))
-
-
-;; gcal synchronization
-(use-package org-gcal
-  :ensure t
-  :config
-  (load "~/.secret/.gcal.el")
-  (org-gcal-fetch)
-  (org-gcal-sync))
-
-
+;;; org-settings ends here
