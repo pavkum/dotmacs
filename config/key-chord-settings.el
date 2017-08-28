9bd6124af9f7852e9e176bae2dc64965e5df4111
Pavan Kumar L <kumarl@adobe.com>
2017-08-28 12:34:37 +0530

    Bug fix

--- config/key-chord-settings.el
+++ config/key-chord-settings.el
@@ -4,6 +4,6 @@
   (key-chord-mode 1)
   (key-chord-define-global ">>" 'persp-next)
   (key-chord-define-global "<<" 'persp-prev)
-  (key-chord-define-global "LL" 'persp-last)
+  (key-chord-define-global "LL" 'persp-prev)
   (key-chord-define-global "KK" 'persp-kill)
   (key-chord-define-global "PP" 'persp-switch))
